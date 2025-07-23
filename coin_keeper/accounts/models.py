ff20cca5afad5cdcaf9703e6317e13a24130cb31
Vyacheslav Parshakov <parsakovslava03@gmail.com>
2025-07-23 16:10:27 +0700

    feat: Add custom user model
    - Add CustomUser
    - Set AUTH_USER_MODEL as CustomUser
    - Make migrations

--- coin_keeper/accounts/models.py
+++ coin_keeper/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
