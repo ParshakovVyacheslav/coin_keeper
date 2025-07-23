52b4496ab87a848bdbe3603557469e1fdd078578
Vyacheslav Parshakov <parsakovslava03@gmail.com>
2025-07-23 13:32:00 +0700

    feat: Add minimal apps
    - Add 'accounts', 'transactions' and 'categories'
    - Add apps to settings.py

--- coin_keeper/coin_keeper/settings.py
+++ coin_keeper/coin_keeper/settings.py
@@ -19,6 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'transactions.apps.TransactionsConfig',
+    'categories.apps.CategoriesConfig'
 ]
 
 MIDDLEWARE = [
