485ba352adca0acb0599a82be1c4a57b432b8713
Vyacheslav Parshakov <parsakovslava03@gmail.com>
2025-07-24 18:31:20 +0700

    fix(settings): Replace accounts before django.contrib.auth

--- coin_keeper/coin_keeper/settings.py
+++ coin_keeper/coin_keeper/settings.py
@@ -13,13 +13,15 @@ DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig',
+    
     'transactions.apps.TransactionsConfig',
     'categories.apps.CategoriesConfig'
 ]
