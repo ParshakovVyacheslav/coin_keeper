d6741332e25a9b1e7ce0e02001ebf55703949fbd
Vyacheslav Parshakov <parsakovslava03@gmail.com>
2025-08-07 20:57:29 +0700

    feat: Add internationalization
    - Add languages, locale path, use l10n and locale middleware to settings.py
    - Create .po file using django-admin makemessages
    - Add translations to .po
    - Create .mo file using django-admin compilemessages
    - Change url paths using django.conf.urls.i18n.i18n_patterns

--- coin_keeper/coin_keeper/settings.py
+++ coin_keeper/coin_keeper/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -96,6 +97,12 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
+
+LANGUAGES = [('en', 'English'), ('ru', 'Русский')]
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'),]
 
 USE_TZ = True
 
--- coin_keeper/coin_keeper/urls.py
+++ coin_keeper/coin_keeper/urls.py
@@ -1,7 +1,8 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
-]
+    path('accounts/', include('accounts.urls')),
+)
